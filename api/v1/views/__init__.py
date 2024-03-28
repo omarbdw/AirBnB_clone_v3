a886ff4ee790c9981c029003b510f9589a3cc68e
omarbdw <omarbdw@yahoo.com>
2024-03-29 00:13:02 +0200

    f

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ This module creates a Blueprint object app_views that handles all views"""
 from flask import Blueprint
-from api.v1.views.index import *
-
+if app_views is not None:
+    from api.v1.views.index import *
+    
+    
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
