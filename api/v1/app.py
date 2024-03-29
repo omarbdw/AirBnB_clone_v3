ef837366734308884e6d0738000874c4dda74e58
omarbdw <omarbdw@yahoo.com>
2024-03-29 13:47:51 +0200

    task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """
 This is the main file for the AirBnB clone v3 API.
 """
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from api.v1.views import app_views
 from models import storage
 from os import getenv
@@ -12,12 +12,21 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 
+
 @app.teardown_appcontext
 def teardown_appcontext(exception):
     """
     Closes the storage connection at the end of the request.
     """
     storage.close()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """
+    Handles 404 errors.
+    """
+    return make_response(jsonify({"e": "Not found"}), 404)
 
 
 if __name__ == "__main__":
